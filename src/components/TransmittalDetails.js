46f25275212fbe4c7e2d0ef96ad6fb18319f4298
Jonas Rafael Sarcilla <jr.sarcilla07@gmail.com>
2021-10-14 12:18:38 +0800

    Update on Transmittal Details Install react-bootstrap-table

--- src/components/TransmittalDetails.js
+++ src/components/TransmittalDetails.js
@@ -1,37 +1,156 @@
 import React from 'react'
-import { Stack, DefaultPalette } from '@fluentui/react'
+import { Label, IconButton, Image, ImageFit } from '@fluentui/react';
+import BootstrapTable from 'react-bootstrap-table-next';
+import company_logo from '../images/company_logo.png';
+import qrcode_logo from '../images/QR_Code.png';
 
-const stackStyles = {
-    root: {
-      background: DefaultPalette.themeTertiary,
-      height: '100vh',
-      width: '100',
-      padding: '10px'
-    },
-  };
-  const stackItemStyles = {
-    root: {
-      alignItems: 'center',
-      background: DefaultPalette.themePrimary,
-      color: DefaultPalette.white,
-      display: 'flex',
-      height: 50,
-      justifyContent: 'center',
-    },
-  };
+const emojiIcon = { 
+  iconName: 'Print'
+ };
+
+ const qrCodeProps = {
+  imageFit: ImageFit.center,
+  width: 60,
+  height: 60,
+  styles: {
+    root: { 
+      margin: '0px 5px 0px 0px'
+    }
+  },
+};
+
+const companyLogoProps = {
+  imageFit: ImageFit.center,
+  width: 40,
+  height: 40,
+  styles: {
+    root: { 
+      margin: '0px 5px 0px 0px'
+    }
+  },
+};
+
+const columns = [
+  {
+    dataField: 'fileName',
+    text: 'File Name'
+  }, 
+  {
+    dataField: 'title',
+    text: 'Title'
+  },
+  {
+    dataField: 'rev',
+    text: 'Rev'
+  },
+  {
+    dataField: 'state',
+    text: 'State'
+  },
+  {
+    dataField: 'status',
+    text: 'Status'
+  },
+  {
+    dataField: 'size',
+    text: 'Size'
+  },
+  {
+    dataField: 'ds',
+    text: ''
+  }
+];
+
+const documents = [
+  {
+    id: 1,
+    fileName: 'P0000-DTR-000',
+    title: 'Doc Title',
+    rev: 'Rev 0',
+    state: 'SHR',
+    status: 'PC Approved For',
+    size: '30 KB'
+  },
+  {
+    id: 1,
+    fileName: 'P0000-DTR-000',
+    title: 'Doc Title',
+    rev: 'Rev 0',
+    state: 'SHR',
+    status: 'S0 Started',
+    size: '60 KB'
+  },
+  {
+    id: 2,
+    fileName: 'P0000-DTR-000',
+    title: 'Doc Title',
+    rev: 'Rev 0',
+    state: 'APP/APS',
+    status: 'S0 Started',
+    size: '120 KB'
+  }
+]
 
 const TransmittalDetails = () => {
     return (
-        <div class="d-flex flex-column bd-highlight" style={{ height: '91vh' }}>
-            <header class="p-2 d-flex justify-content-between">
-                <div>Company Logo</div>
-                <div>Print Page</div>
+        <div class="d-flex flex-column p-3" style={{ height: '91vh', overflowY: 'auto', backgroundColor: '#ffffff !important' }}>
+            <header class="d-flex justify-content-between align-items-center">
+              <div class="d-flex flex-row">
+                <div class="d-flex flex-row align-items-center">
+                      <Image
+                        {...qrCodeProps}
+                        src={qrcode_logo}
+                        alt='Example of the image fit value "center" on an image smaller than the frame.'
+                      />
+                </div>
+                <div class="d-flex flex-row align-items-center">
+                    <div>
+                      <Image
+                        {...companyLogoProps}
+                        src={company_logo}
+                        alt='Example of the image fit value "center" on an image smaller than the frame.'
+                      />
+                    </div>
+                    <div>
+                      <Label style={{fontSize: 24 }}>Aurecon Transmittal</Label>
+                    </div>
+                </div>
+              </div>
+              <div>
+                <IconButton className="printDocument" iconProps={emojiIcon} title="Print Transmittal" ariaLabel="Print Transmittal" disabled={false} checked={false} />
+              </div>
             </header>
-            <section class="p-2 bd-highlight">Transmittal Details</section>
-            <section class="p-2 bd-highlight flex-fill">Documents Table</section>
-            <footer class="p-2 bd-highlight">
-                <p>Disclaimer</p>
-                <cite>** The user agrees to use the drawings/data only for the purpose for which it was develop by Aurecon. A user of Aurecon drawings/data in electronic format accepts the associated risk of using it without taking reasonable measures to verify the accuracy thereof against the original un-editable version, available from Aurecon. Any discrepancy between the drawings/data and the contract shall be finally decided by the Engineer/Principal</cite>
+            
+            <section className="py-3">
+              <div className="d-flex justify-content-between align-items-center">
+                <Label style={{fontSize: 16 }}>Details</Label>
+              </div>
+              <div className="container-fluid">
+                <div class="row" style={{ minWidth: 250, fontSize: 14, border: '2px solid #313f52' }}>
+                  <div class="col-12 col-md-12 col-lg-6">
+                    <p style={{ marginBottom: '0.5em'}}><b>Transmittal:</b> <span>[To Be Generated]</span></p>
+                    <p style={{ marginBottom: '0.5em'}}><b>Sent By:</b> <span>Kimberley Morrisson</span></p>
+                    <p style={{ marginBottom: '0.5em'}}><b>Project:</b> <span>Test Project</span></p>
+                    <p style={{ marginBottom: '0.5em'}}><b>Issued On:</b> <span>Oct 5, 2021</span></p>
+                    <p style={{ marginBottom: '0.5em'}}><b>Key Contact:</b> <span>Eurich De Castro</span></p>
+                  </div>
+                  <div class="col-12 col-md-12 col-lg-6">
+                    <p style={{ marginBottom: '0.5em'}}><b>Subject:</b> <span>UI Review of Transmittal</span></p>
+                    <p style={{ marginBottom: '0.5em'}}><b>Message:</b> <span>** The user agrees to use the drawings/data only for the purpose for which it was develop by Aurecon. A user of Aurecon drawings/data in electronic format accepts the associated risk of using it without taking reasonable measures to verify the accuracy thereof against the original un-editable version, available from Aurecon. Any discrepancy between the drawings/data and the contract shall be finally decided by the Engineer/Principal</span></p>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section class="flex-fill">
+              <div className="d-flex justify-content-between align-items-center">
+                <Label style={{fontSize: 16 }}>Documents</Label>
+                <IconButton className="printDocument" iconProps={{iconName: 'DownloadDocument'}} title="Print Transmittal" ariaLabel="Print Transmittal" disabled={false} checked={false} />
+              </div>
+              <BootstrapTable wrapperClasses="documentTable" keyField="id" data={ documents } columns={ columns } />
+            </section>
+            <footer   class="">
+              <Label style={{fontSize: 16 }}>Disclaimer</Label>
+              <cite style={{fontSize: 12}}>** The user agrees to use the drawings/data only for the purpose for which it was develop by Aurecon. A user of Aurecon drawings/data in electronic format accepts the associated risk of using it without taking reasonable measures to verify the accuracy thereof against the original un-editable version, available from Aurecon. Any discrepancy between the drawings/data and the contract shall be finally decided by the Engineer/Principal</cite>
             </footer>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
